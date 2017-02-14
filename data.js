1d8391cc521a17fcfdf06973a8f08c5f60046c23
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-14 13:42:15 +0700

    Populate initial data

--- data.js
+++ data.js
@@ -1,5 +1,20 @@
+export const thoughtTags = ['Nerd', 'Life', 'Design', 'Business'];
+
+export const workTags = ['Coding', 'Design', 'Management'];
+
 export const thoughts = [
+  {
+    name: 'Good Design',
+    page: '/2017/good-design',
+    date: 'Febuary 14, 2017',
+    tags: ['Design'],
+  },
 ];
 
 export const works = [
+  {
+    name: 'kene.co.th',
+    page: '/works/kene',
+    tags: ['Design', 'Management'],
+  },
 ];
