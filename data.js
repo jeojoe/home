a80d2c601c00b19be28bc656b5d966a84a186e87
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-14 18:47:04 +0700

    Fix data ordering

--- data.js
+++ data.js
@@ -6,28 +6,28 @@ export const tags = {
 
 export const thoughts = [
   {
-    name: 'Good Design',
-    page: '/2017/good-design',
-    date: 'Febuary 14, 2017',
-    tags: ['Design'],
-  },
-  {
     name: 'Next.js : (really) minimalistic React framework',
     page: '/2017/next-js',
     date: 'Febuary 14, 2017',
     tags: ['Nerd'],
   },
+  {
+    name: 'Good Design',
+    page: '/2017/good-design',
+    date: 'Febuary 14, 2017',
+    tags: ['Design'],
+  },
 ];
 
 export const works = [
   {
-    name: 'kene.co.th',
-    page: '/works/kene',
-    tags: ['Design', 'Management'],
-  },
-  {
     name: 'emplist.com',
     page: '/works/emplist',
     tags: ['Coding', 'Design', 'Management'],
+  },
+  {
+    name: 'kene.co.th',
+    page: '/works/kene',
+    tags: ['Design', 'Management'],
   },
 ];
