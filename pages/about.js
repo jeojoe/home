3c50d86bd5e0925a0b9a16200773a269362838ed
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-14 18:48:58 +0700

    Good design

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default () => (
     <p>Born in Thailand on May 31, 1994</p>
     <p>Studying in Computer Engineering, Chulalongkorn University (Final Year)</p>
     <p>Passionate about React + ES6, clean code, optimization and bodybuilding</p>
-    <p>Interested in design, good movies, electronic music, ML</p>
+    <p>Interested in good design, good movies, electronic music, ML</p>
     <p>Love to discuss ideas, philosophy and western history</p>
     <p>(9gag, Skyrim and fast loading site FTW btw lol)</p>
     <div>---</div>
