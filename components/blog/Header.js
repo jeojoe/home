23460a2891f614957889fa461514c1b2e4f29723
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-18 13:49:30 +0700

    Make Header detect tags + add anchor security

--- components/blog/Header.js
+++ components/blog/Header.js
@@ -2,8 +2,9 @@ export default ({ name, date, tags }) => (
   <div>
     <h3>{name}</h3>
     <h6>
-      {`${date} | `}
-      {tags.map((tag, i) =>
+      {date}
+      {tags && ' | '}
+      {tags && tags.map((tag, i) =>
         <span key={i}>{tag}{i === tags.length - 1 ? '' : ', ' } </span>)
       }
     </h6>
--- pages/works/homeservice.js
+++ pages/works/homeservice.js
@@ -14,7 +14,7 @@ export default ({ url }) => {
         />
         <p>At final year in university, i decided to work part-time at software consulting company. i worked on SANSIRI's product called Home Service, the application for your home and your smart living. i was assigned to do backend programming (PHP + MySQL) and also associated project management.</p>
         <p>I also had a responsibility on Frontend programming using JQuery.</p>
-        <p>The app is on both <a href="https://play.google.com/store/apps/details?id=com.homeservice&hl=en" target="_blank">App Store</a> and <a href="https://itunes.apple.com/th/app/home-service/id624978116?mt=8" target="_blank">Play Store</a>.</p>
+        <p>The app is on both <a href="https://itunes.apple.com/th/app/home-service/id624978116?mt=8" target="_blank" rel="noopener noreferrer">App Store</a> and <a href="https://play.google.com/store/apps/details?id=com.homeservice&hl=en" target="_blank" rel="noopener noreferrer">Play Store</a>.</p>
       </div>
     </Layout>
   );
