3ef96dcd031490341b34181b408bb4315ced76e0
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-21 03:31:34 +0700

    Update url

--- pages/works/web-prog.js
+++ pages/works/web-prog.js
@@ -15,6 +15,7 @@ export default ({ url }) => {
         <p>Around March 2016, i studied Web Programming. There were weekly assignments occasionally through out the course. Instructor of this course wants every student to learn the fundamental of building web application (focus on frontend dev.). So he chose very mature tech like jQuery for lecturing and demo. And focus on teaching in-depth basic such as new feature on HTML5 and CSS3, Javascript scope and its asynchronous behaviour, etc.</p>
         <p>All assignments i did are in Github repo. Some of them might not work because of expired free API thought.</p>
         <a href="https://github.com/jeojoe/web-prog" target="_blank" rel="noopener noreferrer">Web Prog Repository (Warning: political sarcastic jokes are strong with resume work !!)</a>
+        <a href="http://fuqdissheet.meteorapp.com" target="__blank" rel="noopener noreferrer">Final Project Demo</a>
       </div>
     </Layout>
   );
