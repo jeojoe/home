81e3232523585a72bc1b194f1a0bb303e3d4e504
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-21 16:29:27 +0700

    Fix message

--- pages/works/ppt-coupler.js
+++ pages/works/ppt-coupler.js
@@ -12,7 +12,7 @@ export default ({ url }) => {
           date={work.date}
           tags={work.tags}
         />
-        <p>ppt-coupler.com is a company website of Panasia Progressive Technology or PPT. PPT is rebar coupler company. The owner of the company is my friend's father. he wants to build a new website presenting the company with modern design that have an impact on customers then hopefully create buying actions, so my friend came for my help.</p>
+        <p>the project was to develop a company website of Panasia Progressive Technology or PPT. PPT is rebar coupler company. The owner of the company is my friend's father. he wanted to build a new website presenting the company with modern design that have an impact on customers then hopefully create buying actions, so my friend came for my help.</p>
         <p>The design direction i took was bold and strong design. I tried to create a powerful look to the site so that it will consequencely build a strong company image that sells.</p>
         <p>I used React (well, of course). But i was tired of bloating React boilerplate. So i looked for new React framework that is minimalistic and easy to use. Then i found Next.js. And my React life has changed since then.</p>
         <p>This site is built on Next.js (React). It's under developement.</p>
