499f860812313113df5cccc30bdfd7f37553a9fd
Jirat Onaree <jirat.onaree@gmail.com>
2017-02-23 14:37:05 +0700

    Fix work link and about

--- pages/about.js
+++ pages/about.js
@@ -3,12 +3,12 @@ import Layout from '../components/Layout';
 export default () => (
   <Layout title="jeojoe | About" subHeader="About">
     <div className="bug-about">
-      <p>Born in Thailand on May 31, 1994</p>
-      <p>Studying in Computer Engineering, Chulalongkorn University (Final Year)</p>
-      <p>Passionate about React + ES6, clean code, optimization and bodybuilding</p>
+      <p>Born in Thailand on May 31, 1994.</p>
+      <p>Studying in Computer Engineering, Chulalongkorn University (Final Year).</p>
+      <p>Passionate about React + ES6, clean code, optimization and bodybuilding.</p>
       <p>Interested in good design and movies, electronic music, ML, and learning German.</p>
       <p>Love to discuss ideas, philosophy and western history.</p>
-      <p>Always find time to pratice coding.</p>
+      <p>Always find time to practice coding.</p>
       <p>(9gag, Skyrim and fast loading site FTW btw)</p>
       <div>---</div>
       <div>
--- pages/works/platonos.js
+++ pages/works/platonos.js
@@ -12,7 +12,7 @@ export default ({ url }) => {
           date={work.date}
           tags={work.tags}
         />
-        <p>Me and <a href="https://github.com/aunnnn" target="_blank">my friend</a> want to build a place where people can discuss about serious topics like science, art, philosophy, etc, Enabling them to connect and get to know each others.</p>
+        <p>Me and <a href="https://github.com/aunnnn" target="_blank">my friend</a> want to build a place where people can discuss about serious topics like science, art, philosophy, etc. Enable them to connect and get to know each others.</p>
         <p>This is the first big website we did so we chose to use Meteor framework for buiding the site. Meteor is a ultra simple javascript framework that you can build the website in no time. For frontend we chose React and application stucture is heavily influenced by Meteor code style guideline. This is the first time that we use linter, ESLint (Airbnb config.), it was the best decision ever in my js journey. I use it on every js projects ever since.</p>
         <p>Meteor has taught us about fundamental concept of building real-time web application and the important of code styling. And working on this project has taught me about tranforming app ideas into a real world application, UI and UX design, and value of teamwork.</p>
         <p>This site is built on Meteor, React and MongoDB. The project isn't finish yet because studying and project stuff but it will be soon.</p>
