eff8ed7b47f68e84cb429711d2ac85cdd6960bcb
Joe Aree <jirat.onaree@gmail.com>
2019-01-02 17:25:21 +0700

    Grammar :fire:

--- pages/works/platonos.js
+++ pages/works/platonos.js
@@ -16,7 +16,7 @@ export default ({ url }) => {
           <img src="https://platonos.com/static/plato-red.jpg" />
           <img src="https://platonos.com/static/plato.jpg" />
         </div>
-        <p>Me and <a href="https://aunnnn.com" target="_blank" rel="noopener noreferrer">my friend</a> at first wanted to build a place where people could discuss about serious topics like science, art, philosophy, etc. Enabling them to connect and get to know each others. Then we decided to built it using Meteor.. worst choice ever made. So we decided to create more lighter version with Next.js and Graph Cool (GraphQL BaaS like Firebase). And also changed from public profile to be completely anonymous. Everybody is Plato (Grey and Red Plato to be specific) talking to each other 1-1 and every discussions are public.</p>
+        <p>Me and <a href="https://aunnnn.com" target="_blank" rel="noopener noreferrer">my friend</a> at first wanted to build a place where people could discuss about serious topics like science, art, philosophy, etc. Enabling them to connect and get to know each others. Then we decided to built it using Meteor.. worst choice ever made. So we decided to create more lighter version with Next.js and Graph Cool (GraphQL BaaS like Firebase). And also changed from public profile to be completely anonymous. Everybody is Plato (Grey and Red Plato to be specific) talking to each other 1-1 and every discussion is public.</p>
         <a href="https://platonos.com/welcome" target="__blank" rel="noopener nonreferrer">platonos.com</a>
         <br />
         <a href="https://github.com/aunnnn/platonos" target="_blank" rel="noopener noreferrer">Source Code</a>
