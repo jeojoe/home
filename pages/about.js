34dfd8bab61f1643f6d4448a896f2673af70998d
Joe Aree <jirat.onaree@gmail.com>
2019-01-02 16:44:05 +0700

    Update about

--- pages/about.js
+++ pages/about.js
@@ -7,13 +7,19 @@ export default () => (
         <img src="/static/memes/1.jpg" style={{ maxWidth: '300px' }} />
         {/* <sub>refresh for more lol</sub> */}
       </div>
-      <p>Born in Thailand on May 31, 1994.</p>
-      <p>Studying in Computer Engineering, Chulalongkorn University (Final Year).</p>
-      <p>Passionate about Full-stack programming, clean code, performance optimization and bodybuilding.</p>
-      <p>Interested in good design and movies, electronic music, ML and learning German.</p>
-      <p>Love to discuss ideas, philosophy and western history.</p>
-      <p>Always find time to learn new things.</p>
-      <p>(9gag, Skyrim and fast loading site FTW btw)</p>
+      <p>Hello There.</p>
+      <p>I'm Joe. Born and live in Thailand, what a beautiful country.</p>
+      <p>Obsessed in Javascript and everything JS related, especially React.</p>
+      <blockquote>
+        "React is such a good idea that we will spend the rest of the decade continuing to explore its implications and applications."
+        <a href="https://twitter.com/rauchg/status/801005961334943744"><cite>@rauchg</cite></a>
+      </blockquote>
+      <p>Totally agreed.</p>
+      <p>Also like to learn other things in computer science though, such as Distributed System, Networking, Cryptography.</p>
+      <p>(Or basically Blockchain, to put it simply)</p>
+      <p>Interested in Dieter Rams design, good movies and also bodybuilding.</p>
+      <p>Love to discuss about ideas, philosophy, western history, politics and good lores (LOTR, Skyrim).</p>
+      <p></p>
       <div>---</div>
       <div>
         <a href="https://github.com/jeojoe" target="_blank" rel="noopener noreferrer">Github</a>
