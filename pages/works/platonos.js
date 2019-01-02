67f433da4b66625a6b27494b7f0eaf70dc56a1a8
Joe Aree <jirat.onaree@gmail.com>
2019-01-02 17:06:29 +0700

    Update work: Platonos

--- pages/works/platonos.js
+++ pages/works/platonos.js
@@ -12,14 +12,24 @@ export default ({ url }) => {
           date={work.date}
           tags={work.tags}
         />
-        <p>Me and <a href="https://github.com/aunnnn" target="_blank" rel="noopener noreferrer">my friend</a> want to build a place where people can discuss about serious topics like science, art, philosophy, etc. Enabling them to connect and get to know each others.</p>
-        <p>This is the first big website we did so we chose to use Meteor framework for buiding the site fast. Meteor is a ultra simple javascript framework that you can build the website in no time. For frontend we chose React and application stucture is heavily influenced by Meteor <a href="https://guide.meteor.com/code-style.html" target="_blank" rel="noopener noreferrer">code style guideline</a>. This is the first time that we use linter, ESLint (Airbnb config.), it was the best decision ever in my js journey. I use it on every js projects ever since.</p>
-        <p>Meteor has taught us about fundamental concept of building real-time web application and the important of code styling. And working on this project has taught me about tranforming app ideas into a real world application, UI and UX design and value of teamwork.</p>
-        <p>This site is built on Meteor, React and MongoDB. The project isn't finish yet because studying and project stuff but it will be soon.</p>
-        <a href="https://github.com/aunnnn/platonos" target="_blank" rel="noopener noreferrer">Repository</a>
+        <div>
+          <img src="https://platonos.com/static/plato-red.jpg" />
+          <img src="https://platonos.com/static/plato.jpg" />
+        </div>
+        <p>Me and <a href="https://github.com/aunnnn" target="_blank" rel="noopener noreferrer">my friend</a> at first wanted to build a place where people could discuss about serious topics like science, art, philosophy, etc. Enabling them to connect and get to know each others. Then we decided to built it using Meteor.. worst choice ever made. So we decided to create more lighter version with Next.js and Graph Cool (GraphQL BaaS like Firebase). And also changed from public profile to be completely anonymous. Everybody is Plato (Grey and Red Plato to be specific) talking to each other 1-1 and every discussions are public.</p>
+        <a href="https://platonos.com/welcome" target="__blank" rel="noopener nonreferrer">platonos.com</a>
+        <br />
+        <a href="https://github.com/aunnnn/platonos" target="_blank" rel="noopener noreferrer">Source Code</a>
         <br /><br />
-        <a href="https://platonos.now.sh" target="__blank" rel="noopener nonreferrer">MVP (Contact me for testing account. You know how to contact me.)</a>
       </div>
+      <style jsx>
+        {`
+          img {
+            width: initial !important;
+            height: 128px;
+          }
+        `}
+      </style>
     </Layout>
   );
 };
