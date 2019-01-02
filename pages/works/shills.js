b68f591ca3f787508244915b686911d2f46031f2
Joe Aree <jirat.onaree@gmail.com>
2019-01-02 17:21:25 +0700

    Update work : shills.lol

--- pages/works/shills.js
+++ pages/works/shills.js
@@ -12,13 +12,22 @@ export default ({ url }) => {
           date={work.date}
           tags={work.tags}
         />
-        <p>KENE Laboratory Co., Ltd. is skincare brand i did with my friend since freshman year. kene.co.th is brand's e-commerce website that me and 2 of my engineering olympiad-genius-level friends team up building it together in junior year.</p>
-        <p>Frankly i didn't touch any code. It was a work of genius by my friends who built it from the first line of code. (Yeah, they didn't use any e-commerce library, just learning e-commerce good practice and security fundamental then built everthing from nothing by their skills). But since i'm project manager i had to design UX/UI, what looks and feels of the site that suit my brand identity and maximize conversion rate. And then managed the team to get the project done.</p>
-        <p>Although i now quit working on this company, it leaves me fundamental skills of building a brand, design, project management, marketing and most importantly, teamwork and friendship. It was one of the best experience in my life.</p>
-        <p>This site is built on Angular, Laravel and MySQL.</p>
-        <a href="https://kene.co.th/" target="_blank" rel="noopener noreferrer">www.kene.co.th</a>
-        <img src="/static/works/kene.jpg" alt="kene.co.th screenshot" />
+        <img src="https://shills.lol/static/lol.png" />
+        <p><b>"DUDE, DID YOU BUY VECHAIN YET?"</b></p>
+        <p><b>"Ahem, execuse me, do you have time to talk about our lord RaiBlocks?"</b></p>
+        <p>Late 2017 and early 2018 was a weird time when shills plagued <a href="https://www.reddit.com/r/CryptoCurrency">/r/CryptoCurrency</a>. So me and <a href="https://github.com/aunnnn" target="_blank" rel="noopener noreferrer">my friend</a> has an idea for a place dedicated to shills! It exploded for a week then faded away though. But at least we had some really good shills that made us laugh.</p>
+        <a href="https://shills.lol/" target="_blank" rel="noopener noreferrer">shills.lol</a>
+        <br />
+        <a href="https://github.com/aunnnn/shills.lol" target="_blank" rel="noopener noreferrer">Source Code</a>
       </div>
+      <style jsx>
+        {`
+          img {
+            width: initial !important;
+            height: 128px;
+          }
+        `}
+      </style>
     </Layout>
   );
 };
