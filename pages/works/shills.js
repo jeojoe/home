452c20698fb9ea602a89189f44aea4c1048d69c2
Joe Aree <jirat.onaree@gmail.com>
2019-01-04 13:58:12 +0700

    Better grammar

--- pages/works/shills.js
+++ pages/works/shills.js
@@ -15,7 +15,7 @@ export default ({ url }) => {
         <img src="https://shills.lol/static/lol.png" />
         <p><b>"DUDE, DID YOU BUY VECHAIN YET?"</b></p>
         <p><b>"Ahem, execuse me, do you have time to talk about our lord RaiBlocks?"</b></p>
-        <p>Late 2017 and early 2018 was a weird time when shills plagued <a href="https://www.reddit.com/r/CryptoCurrency">/r/CryptoCurrency</a>. So me and <a href="https://aunnnn.com" target="_blank" rel="noopener noreferrer">my friend</a> has an idea for a place dedicated to shills! It exploded for a week then faded away though. But at least we had some really good shills that made us laugh.</p>
+        <p>Late 2017 and early 2018 was a weird time when shills plagued <a href="https://www.reddit.com/r/CryptoCurrency">/r/CryptoCurrency</a>. So me and <a href="https://aunnnn.com" target="_blank" rel="noopener noreferrer">my friend</a> had an idea for a place dedicated to shills! It exploded for a week then faded away though. But at least we had some really good shills that made us laugh.</p>
         <a href="https://shills.lol/" target="_blank" rel="noopener noreferrer">shills.lol</a>
         <br />
         <a href="https://github.com/aunnnn/shills.lol" target="_blank" rel="noopener noreferrer">Source Code</a>
