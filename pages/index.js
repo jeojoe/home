c8a9c2baf5786b8ebfb9509f125f28a54c916436
Joe Aree <jirat.onaree@gmail.com>
2019-05-30 23:35:34 +0700

    awesome people

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 import Links from '../components/Links';
 
 export default ({ url }) => (
-  <Layout url={url} title="jeojoe" subHeader="(Currently working with super awesome team at Bitfinex.)">
+  <Layout url={url} title="jeojoe" subHeader="(Currently working with super awesome people at Bitfinex.)">
     <Links
       data={[
         { name: 'Thoughts', page: '/thoughts' },
